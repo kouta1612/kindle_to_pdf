80fdd12470b62b3d4a4c5b538be5b1a1dd0a2bfd
kouta1612 <kouta1612world69@gmail.com>
2026-04-25 16:49:37 +0900

    refactor

--- screenshot.py
+++ screenshot.py
@@ -4,7 +4,7 @@ import os
 
 # --- ここを編集してください ---
 # 撮影したい総ページ数を設定
-TOTAL_PAGES = 10
+TOTAL_PAGES = 1
 
 # スクリーンショットを保存するフォルダ名
 OUTPUT_FOLDER = "book_screenshots"
@@ -13,27 +13,39 @@ OUTPUT_FOLDER = "book_screenshots"
 WAIT_TIME = 2.5
 # -----------------------------
 
-# デスクトップに保存先フォルダを作成
-desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
-output_dir = os.path.join(desktop_path, OUTPUT_FOLDER)
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+def create_dir(folder_name: str) -> str:
+    desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+    output_dir = os.path.join(desktop_path, folder_name)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    return output_dir
 
-print(">>> 5秒後に自動撮影を開始します。")
-print(">>> Kindleアプリをフルスクリーンで最前面に表示してください。")
-time.sleep(5)
+def screenshot(dir_path: str):
+    for i in range(TOTAL_PAGES):
+        page_num = i + 1
+        print(f">>> {page_num} / {TOTAL_PAGES} ページ目を撮影中...")
 
-for i in range(TOTAL_PAGES):
-    page_num = i + 1
-    print(f">>> {page_num} / {TOTAL_PAGES} ページ目を撮影中...")
+        screenshot = pyautogui.screenshot()
+        file_path = os.path.join(dir_path, f"page_{page_num:04d}.png")
+        screenshot.save(file_path)
 
-    screenshot = pyautogui.screenshot()
-    file_path = os.path.join(output_dir, f"page_{page_num:04d}.png")
-    screenshot.save(file_path)
+        pyautogui.press('left') 
+        time.sleep(WAIT_TIME)
 
-    # left or right
-    pyautogui.press('left') 
-    time.sleep(WAIT_TIME)
+def main():
+    # デスクトップに保存先フォルダを作成
+    dir_path = create_dir(OUTPUT_FOLDER)
 
-print(f">>> 撮影が完了しました。")
-print(f"デスクトップの '{OUTPUT_FOLDER}' フォルダに画像が保存されました。")
+    # スクショ準備
+    print(">>> 5秒後に自動撮影を開始します。")
+    print(">>> Kindleアプリをフルスクリーンで最前面に表示してください。")
+    time.sleep(5)
+
+    # スクショ
+    screenshot(dir_path)
+
+    print(f">>> 撮影が完了しました。")
+    print(f"デスクトップの '{OUTPUT_FOLDER}' フォルダに画像が保存されました。")
+
+if __name__ == "__main__":
+    main()
